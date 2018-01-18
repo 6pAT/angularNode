7449e81ead233514f173f56b98f481fe600686f1
Eugen Harchishin <6pat.ua@gmail.com>
2018-01-18 16:40:50 +0200

    update app.component and index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { EventsformComponent } from './components/eventsform/eventsform.component';
+import { NewComponentComponent } from './components/new-component/new-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsformComponent,
+    NewComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
